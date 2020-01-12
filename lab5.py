e56ff4e005b823086fa0f4e5a54de7d3adbefd8b
ndy-cd <nody.email@gmail.com>
2020-01-12 18:35:50 +0500

    Create lab5.py

---
+++ lab5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+#используемые величины
+m = 9.11e-28
+e = 4.8e-10
+c = 3e10
+erstd = 0.01256637
+sgse = 3.3e-5
+B = 10 * erstd
+E = 10 * sgse
+
+#
+
